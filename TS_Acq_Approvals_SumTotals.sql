d5d705a36d5297d8662de460fb3fe1e2a9be6eb4
Lauren Schiller <lschiller@uchicago.edu>
2026-09-14 14:04:24 -0500

    Update TS_Acq_Approvals_SumTotals.sql

--- TS_Acq_Approvals_SumTotals.sql
+++ TS_Acq_Approvals_SumTotals.sql
@@ -1,7 +1,7 @@
 -- metadb:function TS_Acq_Approvals_SumTotals
 DROP FUNCTION IF EXISTS TS_Acq_Approvals_SumTotals(date);
 
-CREATE OR REPLACE FUNCTION TS_Acq_Approvals_SumTotals(
+CREATE FUNCTION TS_Acq_Approvals_SumTotals(
     run_date date DEFAULT current_date
 )
 RETURNS TABLE (
